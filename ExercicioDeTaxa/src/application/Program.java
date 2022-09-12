20921a2e5d168e3c1782c91bb4482106255e327f
Keidi-Teixeira <99774507+Keidi-Teixeira@users.noreply.github.com>
2022-09-12 17:56:12 -0300

    Update Program.java

--- ExercicioDeTaxa/src/application/Program.java
+++ ExercicioDeTaxa/src/application/Program.java
@@ -1,11 +1,9 @@
 package application;
-
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-
 import entities.Company;
 import entities.Individual;
 import entities.TaxPayer;
